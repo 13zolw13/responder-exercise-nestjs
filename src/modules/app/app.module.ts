bab5caad589d7424ae635d57dbc370e807bbc9a3
tomek <tomaszpawelkowalski@gmail.com>
2022-06-03 17:53:28 +0200

    feat: set up ConfigModule

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { QuestionsModule } from '../questions/questions.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+      cache: true,
+    }),
+
+    QuestionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
