e6cfa33d2963c943fdbe462be8728f3b61e98a16
tomek <tomaszpawelkowalski@gmail.com>
2022-06-04 10:04:14 +0200

    test: create test to get all guestions

--- src/modules/questions/questions.service.spec.ts
+++ src/modules/questions/questions.service.spec.ts
@@ -1,11 +1,24 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from '../database/database.module';
+import { Question } from './entities/question.entity';
 import { QuestionsService } from './questions.service';
 
 describe('QuestionsService', () => {
   let service: QuestionsService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: ['.env'],
+          isGlobal: true,
+          cache: true,
+        }),
+        DatabaseModule,
+        TypeOrmModule.forFeature([Question]),
+      ],
       providers: [QuestionsService],
     }).compile();
 
@@ -14,5 +27,12 @@ describe('QuestionsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Name of the group', () => {
+    it('should be defined', async () => {
+      const response = await service.getQuestions();
+      expect(response).toStrictEqual([]);
+    });
   });
 });
