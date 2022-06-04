a59b39088d1d024b1f2b5609f70ebce17d38a805
tomek <tomaszpawelkowalski@gmail.com>
2022-06-04 16:39:05 +0200

    test: case add new question to db

--- src/modules/questions/questions.service.spec.ts
+++ src/modules/questions/questions.service.spec.ts
@@ -1,10 +1,12 @@
+import { faker } from '@faker-js/faker';
 import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { cleanupBeforeEachSpec } from '../../utils/dbCleaner';
 import { DatabaseModule } from '../database/database.module';
+import { CreateQuestionDto } from './dto/questionDto';
 import { Question } from './entities/question.entity';
 import { QuestionsService } from './questions.service';
-
 describe('QuestionsService', () => {
   let service: QuestionsService;
 
@@ -24,15 +26,28 @@ describe('QuestionsService', () => {
 
     service = module.get<QuestionsService>(QuestionsService);
   });
-
+  cleanupBeforeEachSpec();
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  describe('Name of the group', () => {
-    it('should be defined', async () => {
-      const response = await service.getQuestions();
-      expect(response).toStrictEqual([]);
+  describe('createQuestion', () => {
+    it('should add new question', async () => {
+      const fakeQuestion: CreateQuestionDto = {
+        summary: faker.lorem.sentence(),
+        author: faker.name.firstName(),
+      };
+
+      const response = await service.createQuestion(fakeQuestion);
+      expect(response).toEqual(expect.objectContaining(fakeQuestion));
+    });
+  });
+  describe('GetQuestions', () => {
+    describe('Empty db', () => {
+      it('Should return empty array', async () => {
+        const response = await service.getQuestions();
+        expect(response).toMatchSnapshot();
+      });
     });
   });
 });
