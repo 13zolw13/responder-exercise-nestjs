7723f6a158449f6ed425454d643e198bfe3ad3d8
tomek <tomaszpawelkowalski@gmail.com>
2022-06-04 16:57:57 +0200

    test(e2e): add  test for  post question

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,8 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from '../src/modules/app/app.module';
+import { CreateQuestionDto } from '../src/modules/questions/dto/questionDto';
+import { cleanupBeforeEachSpec } from '../src/utils/dbCleaner';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -18,8 +20,22 @@ describe('AppController (e2e)', () => {
   it('/ (GET)', () => {
     return appGet().get('/').expect(200).expect('Hello World!');
   });
+  cleanupBeforeEachSpec();
 
-  it('/questions (GET)', async () => {
-    return appGet().get('/questions').expect(200).expect([]);
+  describe('/questions', () => {
+    describe('POST', () => {
+      it('should add new question', async () => {
+        const fakeQuestion: CreateQuestionDto = {
+          summary: 'test',
+          author: 'John Doe',
+        };
+        return appGet().post('/questions').send(fakeQuestion).expect(201);
+      });
+    });
+    describe('(GET)', () => {
+      it('should return empty table', async () => {
+        return appGet().get('/questions').expect(200).expect([]);
+      });
+    });
   });
 });
