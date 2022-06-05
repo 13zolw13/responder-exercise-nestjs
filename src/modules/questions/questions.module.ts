b055b6d93360d7644ff11f546c013f107fdc4ba7
tomek <tomaszpawelkowalski@gmail.com>
2022-06-05 22:21:57 +0200

    fix: update module with new entity

--- src/modules/questions/questions.module.ts
+++ src/modules/questions/questions.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Answer } from './entities/answers.entity';
 import { Question } from './entities/question.entity';
 import { QuestionsController } from './questions.controller';
 import { QuestionsService } from './questions.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Question])],
+  imports: [TypeOrmModule.forFeature([Question, Answer])],
   controllers: [QuestionsController],
   providers: [QuestionsService],
 })
