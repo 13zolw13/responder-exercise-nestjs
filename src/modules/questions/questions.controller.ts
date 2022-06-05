4b7e2aab424b0fc51b7fb97520e4c76853faf0cb
tomek <tomaszpawelkowalski@gmail.com>
2022-06-05 22:32:02 +0200

    feat: create endpoint for adding answer to a question

--- src/modules/questions/questions.controller.ts
+++ src/modules/questions/questions.controller.ts
@@ -6,6 +6,7 @@ import {
   Post,
   ValidationPipe,
 } from '@nestjs/common';
+import { CreateAnswerDto } from './dto/answerDto';
 import { CreateQuestionDto } from './dto/questionDto';
 import { QuestionsService } from './questions.service';
 
@@ -26,5 +27,17 @@ export class QuestionsController {
   @Get('/:questionId')
   async findQuestionById(@Param('questionId') questionId) {
     return await this.questionsService.findQuestionById(questionId);
+  }
+
+  @Get('/:questionId/answers')
+  async findQuestionsWithAnswers(@Param('questionId') questionId) {
+    return await this.questionsService.findQuestionsWithAnswers(questionId);
+  }
+
+  @Post('/:questionId/answers')
+  async AddAnswers(
+    @Body(new ValidationPipe()) createAnswerDto: CreateAnswerDto,
+  ) {
+    return await this.questionsService.addAnswer(createAnswerDto);
   }
 }
