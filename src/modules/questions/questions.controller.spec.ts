9456ccc6407066c7833a1b3459e256670c18c868
tomek <tomaszpawelkowalski@gmail.com>
2022-06-09 12:40:54 +0200

    test: set up controller test, add case getQuestions

--- src/modules/questions/questions.controller.spec.ts
+++ src/modules/questions/questions.controller.spec.ts
@@ -1,20 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QuestionsController } from './questions.controller';
 import { QuestionsService } from './questions.service';
+import { MockQuestions } from './tests/mockQuestionDto';
 
 describe('QuestionsController', () => {
   let controller: QuestionsController;
+  class QuestionsServiceMock {
+    createQuestion() {
+      return {};
+    }
+    getQuestions() {
+      return MockQuestions;
+    }
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [QuestionsController],
       providers: [QuestionsService],
-    }).compile();
+    })
+      .overrideProvider(QuestionsService)
+      .useClass(QuestionsServiceMock)
+      .compile();
 
     controller = module.get<QuestionsController>(QuestionsController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be defined', () => {
+    expect(controller.getQuestions()).toBeDefined();
+    console.log(controller.getQuestions());
+    expect(controller.getQuestions()).toEqual([...MockQuestions]);
   });
 });
