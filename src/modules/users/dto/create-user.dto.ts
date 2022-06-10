7bef6afa663350e608c2822fbc034265d06ad2e5
tomek <tomaszpawelkowalski@gmail.com>
2022-06-10 10:10:09 +0200

    feat: new method create new user

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, Min } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  username: string;
+  @IsString()
+  @IsEmail()
+  email: string;
+  @IsString()
+  @Min(4)
+  password: string;
+}
