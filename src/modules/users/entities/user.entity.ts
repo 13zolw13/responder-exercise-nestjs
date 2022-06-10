7f2d775684d4de68fb9847089a62e8a99c66ea4d
tomek <tomaszpawelkowalski@gmail.com>
2022-06-10 08:30:27 +0200

    feat: add user entity

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,4 +1,23 @@
+<<<<<<< HEAD
 import { Entity } from 'typeorm';
 
 @Entity({ name: 'users' })
 export class User {}
+=======
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
+>>>>>>> feat: add user entity
