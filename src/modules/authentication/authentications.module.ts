e41f9271d9681a82fae173f8225624b13a139f37
tomek <tomaszpawelkowalski@gmail.com>
2022-06-14 14:32:04 +0200

    refactor: jwt configuration

--- src/modules/authentication/authentications.module.ts
+++ src/modules/authentication/authentications.module.ts
@@ -2,24 +2,18 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
-import { JwtStrategy } from './authentication.jwt.strategy';
-import { LocalStrategy } from './authentication.local.strategy';
 import { AuthenticationsController } from './authentications.controller';
 import { AuthenticationsService } from './authentications.service';
+import { jwtConfig } from './jwt.factory.config';
+import { JwtStrategy } from './strategies/authentication.jwt.strategy';
+import { LocalStrategy } from './strategies/authentication.local.strategy';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
     JwtModule.registerAsync({
-      useFactory() {
-        return {
-          secret: process.env.JWT_SECRET,
-          signOptions: {
-            expiresIn: '1d',
-          },
-        };
-      },
+      useFactory: jwtConfig,
     }),
   ],
   controllers: [AuthenticationsController],
