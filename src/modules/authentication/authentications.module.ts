cdf7da57a4c4d22448a506227064350c68028d24
tomek <tomaszpawelkowalski@gmail.com>
2022-06-14 13:10:52 +0200

    feat: set up passport-jwt

--- src/modules/authentication/authentications.module.ts
+++ src/modules/authentication/authentications.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
+import { JwtStrategy } from './authentication.jwt.strategy';
 import { LocalStrategy } from './authentication.local.strategy';
 import { AuthenticationsController } from './authentications.controller';
 import { AuthenticationsService } from './authentications.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory() {
+        return {
+          secret: process.env.JWT_SECRET,
+          signOptions: {
+            expiresIn: '1d',
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AuthenticationsController],
-  providers: [AuthenticationsService, LocalStrategy],
+  providers: [AuthenticationsService, LocalStrategy, JwtStrategy],
 })
 export class AuthenticationsModule {}
--- src/modules/authentication/authentications.service.ts
+++ src/modules/authentication/authentications.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as argon2 from 'argon2';
-import { User } from '../users/entities/user.entity';
 import { UsersService } from '../users/users.service';
 @Injectable()
 export class AuthenticationsService {
@@ -17,5 +16,8 @@ export class AuthenticationsService {
     }
     return null;
   }
-
+  async login(user: any): Promise<any> {
+    const payload = { username: user.username, sub: user.id };
+    return { access_token: this.jwtService.sign(payload) };
+  }
 }
