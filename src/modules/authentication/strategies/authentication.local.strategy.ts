0864c4102ecb127813aee795924d4fd3637797a1
tomek <tomaszpawelkowalski@gmail.com>
2022-06-14 14:03:18 +0200

    refactor: move gurds and strategies to new folders

--- src/modules/authentication/strategies/authentication.local.strategy.ts
+++ src/modules/authentication/strategies/authentication.local.strategy.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
-import { AuthenticationsService } from './authentications.service';
+import { AuthenticationsService } from '../authentications.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthenticationsService) {
