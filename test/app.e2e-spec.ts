22fa21b87ddb62c1fdcbcf9c76f88df31d828d4e
tomek <tomaszpawelkowalski@gmail.com>
2022-06-16 22:55:17 +0200

    test(e2): should return users

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -8,6 +8,7 @@ import { JwtStrategy } from '../src/modules/authentication/strategies/authentica
 import { JwtStrategyMock } from '../src/modules/authentication/strategies/mock.jwt.strategy';
 import { CreateAnswerDto } from '../src/modules/questions/dto/answer.dto';
 import { CreateQuestionDto } from '../src/modules/questions/dto/question.dto';
+import { CreateUserDto } from '../src/modules/users/dto/create-user.dto';
 import { cleanupBeforeEachSpec } from '../src/utils/dbCleaner';
 
 describe('e2e with mock jwt', () => {
@@ -38,129 +39,165 @@ describe('e2e with mock jwt', () => {
   beforeEach(async () => {
     question = await stabQuestion();
   });
-
-  describe('/questions', () => {
-    describe('POST', () => {
-      it('should add new question', async () => {
-        await appGet().post('/questions').send(fakeQuestion).expect(201);
+  describe('Public', () => {
+    describe('/questions', () => {
+      describe('POST', () => {
+        it('should add new question', async () => {
+          await appGet().post('/questions').send(fakeQuestion).expect(201);
+        });
+      });
+      describe('(GET)', () => {
+        it('should return array of questions', async () => {
+          const response = await appGet().get('/questions');
+          expect(response.body.length).toBeGreaterThan(0);
+          expect(response.body).toBeInstanceOf(Array);
+          expect(response.body).toEqual([
+            expect.objectContaining({ ...fakeQuestion }),
+          ]);
+        });
       });
     });
-    describe('(GET)', () => {
-      it('should return array of questions', async () => {
-        const response = await appGet().get('/questions');
-        expect(response.body.length).toBeGreaterThan(0);
-        expect(response.body).toBeInstanceOf(Array);
-        expect(response.body).toEqual([
-          expect.objectContaining({ ...fakeQuestion }),
-        ]);
+    describe('/questions/:questionId', () => {
+      describe('GET', () => {
+        it('should return question', async () => {
+          const questionId = question.body.id;
+          const response = await appGet()
+            .get(`/questions/${questionId}`)
+            .expect(200);
+          expect(response.body).toEqual(expect.objectContaining(fakeQuestion));
+        });
+
+        it('should throw error-wrong id', async () => {
+          const response = await appGet().get(
+            `/questions/${faker.datatype.uuid() + 1}`,
+          );
+
+          expect(response.body.message).toBe('Internal server error');
+          expect(response.status).toBe(500);
+        });
+
+        it('should return undefined', async () => {
+          const response = await appGet().get(
+            `/questions/${faker.datatype.uuid()}`,
+          );
+          expect(response.status).toBe(200);
+          expect(response.body).toStrictEqual({});
+        });
+      });
+    });
+
+    describe('/questions/:questionId/answers', () => {
+      describe('(POST)', () => {
+        it('should add new answer', async () => {
+          const fakeAnswer: CreateAnswerDto = {
+            summary: faker.lorem.sentence(),
+            author: faker.name.firstName() + ' ' + faker.name.lastName(),
+            questionId: question.body.id,
+          };
+          const response = await appGet()
+            .post(`/questions/${fakeAnswer.questionId}/answers`)
+            .send(fakeAnswer)
+            .expect(201);
+        });
+      });
+      describe('(GET)', () => {
+        it('should return empty table', async () => {
+          const response = await appGet().get(
+            `/questions/${question.body.id}/answers`,
+          );
+          expect(response.body).toEqual([]);
+        });
+
+        it('should return table', async () => {
+          const fakeAnswer: CreateAnswerDto = {
+            summary: faker.lorem.sentence(),
+            author: faker.name.firstName() + ' ' + faker.name.lastName(),
+            questionId: question.body.id,
+          };
+          const answer = await appGet()
+            .post(`/questions/${fakeAnswer.questionId}/answers`)
+            .send(fakeAnswer)
+            .expect(201);
+
+          const response = await appGet()
+            .get(`/questions/${fakeAnswer.questionId}/answers`)
+            .expect(200);
+          expect(response.body).toEqual(
+            expect.arrayContaining([expect.objectContaining(fakeAnswer)]),
+          );
+        });
+      });
+    });
+    describe('/questions/:questionsId/answers/:answerId', () => {
+      describe('(GET)', () => {
+        it('should return answer', async () => {
+          const fakeAnswer: CreateAnswerDto = {
+            summary: faker.lorem.sentence(),
+            author: faker.name.firstName() + ' ' + faker.name.lastName(),
+            questionId: question.body.id,
+          };
+          const answer = await appGet()
+            .post(`/questions/${fakeAnswer.questionId}/answers`)
+            .send(fakeAnswer)
+            .expect(201);
+          const response = await appGet()
+            .get(
+              `/questions/${fakeAnswer.questionId}/answers/${answer.body.id}`,
+            )
+            .expect(200);
+          expect(response.body).toEqual(expect.objectContaining(fakeAnswer));
+        });
+        it('should return error ', async () => {
+          const question = await appGet().post('/questions').send(fakeQuestion);
+          const fakeAnswer: CreateAnswerDto = {
+            summary: faker.lorem.sentence(),
+            author: faker.name.firstName() + ' ' + faker.name.lastName(),
+            questionId: question.body.id,
+          };
+          const answer = await appGet()
+            .post(`/questions/${fakeAnswer.questionId}/answers`)
+            .send(fakeAnswer)
+            .expect(201);
+          const fakeID = faker.datatype.uuid();
+          const response = await appGet()
+            .get(`/questions/${fakeAnswer.questionId}/answers/${fakeID}`)
+            .expect(500);
+          expect(response.body.message).toBe('Internal server error');
+          expect(response.status).toBe(500);
+        });
+      });
+    });
+    describe('/users/signup', () => {
+      describe('(POST)', () => {
+        it('should create new user', async () => {
+          const response = await stabMockUser(appGet);
+          expect(response.status).toBe(201);
+        });
       });
     });
   });
-  describe('/questions/:questionId', () => {
-    describe('GET', () => {
-      it('should return question', async () => {
-        const questionId = question.body.id;
-        const response = await appGet()
-          .get(`/questions/${questionId}`)
-          .expect(200);
-        expect(response.body).toEqual(expect.objectContaining(fakeQuestion));
-      });
-
-      it('should throw error-wrong id', async () => {
-        const response = await appGet().get(
-          `/questions/${faker.datatype.uuid() + 1}`,
-        );
-
-        expect(response.body.message).toBe('Internal server error');
-        expect(response.status).toBe(500);
-      });
-
-      it('should return undefined', async () => {
-        const response = await appGet().get(
-          `/questions/${faker.datatype.uuid()}`,
-        );
-        expect(response.status).toBe(200);
-        expect(response.body).toStrictEqual({});
-      });
+  describe.only('Private', () => {
+    const userId = faker.datatype.uuid();
+    const username = faker.name.firstName();
+    beforeEach(async () => {
+      await stabMockUser(appGet);
     });
-  });
-
-  describe('/questions/:questionId/answers', () => {
-    describe('(POST)', () => {
-      it('should add new answer', async () => {
-        const fakeAnswer: CreateAnswerDto = {
-          summary: faker.lorem.sentence(),
-          author: faker.name.firstName() + ' ' + faker.name.lastName(),
-          questionId: question.body.id,
-        };
-        const response = await appGet()
-          .post(`/questions/${fakeAnswer.questionId}/answers`)
-          .send(fakeAnswer)
-          .expect(201);
-      });
-    });
-    describe('(GET)', () => {
-      it('should return empty table', async () => {
-        const response = await appGet().get(
-          `/questions/${question.body.id}/answers`,
-        );
-        expect(response.body).toEqual([]);
-      });
-
-      it('should return table', async () => {
-        const fakeAnswer: CreateAnswerDto = {
-          summary: faker.lorem.sentence(),
-          author: faker.name.firstName() + ' ' + faker.name.lastName(),
-          questionId: question.body.id,
-        };
-        const answer = await appGet()
-          .post(`/questions/${fakeAnswer.questionId}/answers`)
-          .send(fakeAnswer)
-          .expect(201);
-
-        const response = await appGet()
-          .get(`/questions/${fakeAnswer.questionId}/answers`)
-          .expect(200);
-        expect(response.body).toEqual(
-          expect.arrayContaining([expect.objectContaining(fakeAnswer)]),
-        );
-      });
-    });
-  });
-  describe('/questions/:questionsId/answers/:answerId', () => {
-    describe('(GET)', () => {
-      it('should return answer', async () => {
-        const fakeAnswer: CreateAnswerDto = {
-          summary: faker.lorem.sentence(),
-          author: faker.name.firstName() + ' ' + faker.name.lastName(),
-          questionId: question.body.id,
-        };
-        const answer = await appGet()
-          .post(`/questions/${fakeAnswer.questionId}/answers`)
-          .send(fakeAnswer)
-          .expect(201);
-        const response = await appGet()
-          .get(`/questions/${fakeAnswer.questionId}/answers/${answer.body.id}`)
-          .expect(200);
-        expect(response.body).toEqual(expect.objectContaining(fakeAnswer));
-      });
-      it('should return error ', async () => {
-        const question = await appGet().post('/questions').send(fakeQuestion);
-        const fakeAnswer: CreateAnswerDto = {
-          summary: faker.lorem.sentence(),
-          author: faker.name.firstName() + ' ' + faker.name.lastName(),
-          questionId: question.body.id,
-        };
-        const answer = await appGet()
-          .post(`/questions/${fakeAnswer.questionId}/answers`)
-          .send(fakeAnswer)
-          .expect(201);
-        const fakeID = faker.datatype.uuid();
-        const response = await appGet()
-          .get(`/questions/${fakeAnswer.questionId}/answers/${fakeID}`)
-          .expect(500);
-        expect(response.body.message).toBe('Internal server error');
-        expect(response.status).toBe(500);
+    describe('/users', () => {
+      describe('(GET)', () => {
+        it('should return unauthorized', async () => {
+          const response = await appGet().get('/users');
+          expect(response.status).toBe(401);
+          expect(response.body.message).toBe('Unauthorized');
+        });
+        it('should return users', async () => {
+          const response = await asAuthorizedUser(
+            appGet().get('/users'),
+            userId,
+            username,
+          );
+          expect(response.status).toBe(200);
+          expect(response.body).toEqual(expect.arrayContaining([]));
+        });
       });
     });
   });
@@ -178,6 +215,14 @@ const createToken = (userId: string, username: string) => {
     JwtStrategyMock.SECRET_MOCK,
   );
 };
+
+async function stabMockUser(appGet: () => request.SuperTest<request.Test>) {
+  const createUserDto = new CreateUserDto();
+  createUserDto.email = faker.internet.email();
+  createUserDto.password = faker.internet.password();
+  createUserDto.username = faker.name.firstName();
+  return await appGet().post('/users/signup').send(createUserDto).expect(201);
+}
 
 function asAuthorizedUser(fn: request.Test, userId: string, username: string) {
   return fn.set({ Authorization: `Bearer ${createToken(userId, username)}` });
